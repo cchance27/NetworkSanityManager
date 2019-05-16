e4b54d55dd79200c8e27dcde65b5b722800e5387
Chris Chance <cchance@gmail.com>
2019-05-16 14:28:02 -0400

    Factored out the main routines in the program to methods just to be a bit more readable. Overkill but i like it.

--- NetworkSanityManager/Program.cs
+++ NetworkSanityManager/Program.cs
@@ -4,52 +4,50 @@ using System.Linq;
 
 namespace NetworkSanityManager
 {
-    class Program
+    internal class Program
     {
         public static Configuration _config;
-        //TODO: Add override to config to "wait for keypress on each plugin step, for debugging, or maybe when debug = true"
-        //TODO: Autoloading plugins, plugins do the cleanup of DeviceResults?
-        //TODO: Plugins need priority (so for instance we can have DNS always run first)
-        //TODO: Plugins can be alerted if theirs a "plugin block" from a previous plugin, and if they care they can abort running.
-        //TODO: Implement logging properly
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            // Load general program configuration
             _config = new Configuration();
+
+            // Convert configuration Subnets to List of IPs
+            var IPList = IPTools.SubnetsToIPAddresses(_config.Settings.Subnets);
+
+            // Some CLI Header Information
             Console.WriteLine($"Threads for Processing: {_config.Settings.Threads}");
             Console.WriteLine($"Domain Name: {_config.Settings.DomainName}");
-
-            // Convert Subnets to List of IPs
-            var IPList = IPTools.SubnetsToIPAddresses(_config.Settings.Subnets);
             Console.WriteLine($"Total IPs for Processing: {IPList.Count}");
             Console.WriteLine();
 
             // Build a list based on subnet and return a list of IPs responding to pings.
-            IPAddressValue[] ActiveIPs;
-            using (var progress = new ProgressBar())
-            {
-                Console.WriteLine("Testing for Active IPs... ");
-                 ActiveIPs = IPTools.BuildActiveIPList(IPList, _config.Settings.Threads, progress);
-                
-            };
-            Console.WriteLine($"Total Active IPs Found: {ActiveIPs.Count()}");
-            Console.WriteLine();
+            IPAddressValue[] ActiveIPs = findActiveIPsWithProgress(IPList);
 
             // Run our SNMP Fetches against all known active IPs
-            Device[] DeviceResults;
-            using (var progress = new ProgressBar())
-            {
-                Console.WriteLine("Checking SNMP for Active IPs... ");
-                var snmp = new SnmpTools(_config);
-                DeviceResults = snmp.GetSnmpDevicesParallel(ActiveIPs, progress);
-            }
-            Console.WriteLine($"Total valid SNMP Responses: {DeviceResults.Where(r => String.IsNullOrWhiteSpace(r.Errors)).ToArray<Device>().Count()}");
-            Console.WriteLine();
+            Device[] DeviceResults = fetchSNMPWithProgress(ActiveIPs);
 
             // Save our results to static files (CSV)
+            // TODO: Output to CSV should be shifted to a plugin it's just another "output" like the rest, just happens to be local.
             Console.WriteLine("Saving Results of Scan");
             ResultStorage.SaveResults(DeviceResults);
             Console.WriteLine();
+
+            runPlugins(DeviceResults);
+        }
+
+        /// <summary>
+        /// Run our plugins for output methods
+        /// </summary>
+        /// <param name="DeviceResults"></param>
+        private static void runPlugins(Device[] DeviceResults)
+        {
+            //TODO: Add override to config to "wait for keypress on each plugin step, for debugging, or maybe when debug = true"
+            //TODO: Autoloading plugins, plugins do the cleanup of DeviceResults?
+            //TODO: Plugins need priority (so for instance we can have DNS always run first)
+            //TODO: Plugins can be alerted if theirs a "plugin block" from a previous plugin, and if they care they can abort running.
+            //TODO: Implement logging properly
 
             Console.WriteLine("Running Plugins");
             var pluginBlocked = false;
@@ -106,7 +104,6 @@ namespace NetworkSanityManager
                 Console.WriteLine("\n" + libre.Commit());
                 Console.WriteLine("\nPress Key to Continue...");
                 Console.ReadKey();
-
             }
 
             if (_config.Settings.Plugins.Contains("oxidized") && !pluginBlocked)
@@ -150,6 +147,46 @@ namespace NetworkSanityManager
                 Console.WriteLine("\nPress Key to Continue...");
                 Console.ReadKey();
             }
+        }
+
+
+        /// <summary>
+        /// Takes a list of IPAddressValues and checks them via ping if they are active with retries.
+        /// with Progress indicators for CLI mode
+        /// </summary>
+        /// <param name="IPList"></param>
+        /// <returns></returns>
+        private static IPAddressValue[] findActiveIPsWithProgress(List<IPAddressValue> IPList)
+        {
+            IPAddressValue[] ActiveIPs;
+            using (var progress = new ProgressBar())
+            {
+                Console.WriteLine("Testing for Active IPs... ");
+                ActiveIPs = IPTools.BuildActiveIPList(IPList, _config.Settings.Threads, progress);
+            };
+            Console.WriteLine($"Total Active IPs Found: {ActiveIPs.Count()}");
+            Console.WriteLine();
+            return ActiveIPs;
+        }
+
+        /// <summary>
+        /// Fetch all active ip's snmp and return a an array of Device[] 
+        /// with progress indicators for CLI mode
+        /// </summary>
+        /// <param name="ActiveIPs"></param>
+        /// <returns></returns>
+        private static Device[] fetchSNMPWithProgress(IPAddressValue[] ActiveIPs)
+        {
+            Device[] DeviceResults;
+            using (var progress = new ProgressBar())
+            {
+                Console.WriteLine("Checking SNMP for Active IPs... ");
+                var snmp = new SnmpTools(_config);
+                DeviceResults = snmp.GetSnmpDevicesParallel(ActiveIPs, progress);
+            }
+            Console.WriteLine($"Total valid SNMP Responses: {DeviceResults.Where(r => String.IsNullOrWhiteSpace(r.Errors)).ToArray<Device>().Count()}");
+            Console.WriteLine();
+            return DeviceResults;
         }
     }
 }
